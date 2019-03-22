e1dbc2be0324e8c359ae642de5031896a3c5141b
Justin Bae <bae_justin@yahoo.com>
2019-03-22 23:55:17 +0000

    change mongodb user password

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index");
     
-mongoose.connect("mongodb+srv://bae_justin:Megaman911?@cluster0-dpefs.mongodb.net/yelp_camp?retryWrites=true", {useNewUrlParser: true});
+mongoose.connect("mongodb+srv://bae_justin:megaman@cluster0-dpefs.mongodb.net/yelp_camp?retryWrites=true", {useNewUrlParser: true});
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
