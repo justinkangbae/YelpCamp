32152a753ba1746df8db7af2f54ecd14b7045df6
Justin Bae <bae_justin@yahoo.com>
2019-03-22 23:44:18 +0000

    add semicolon

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ var express     = require("express"),
 //requiring routes
 var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
-    indexRoutes      = require("./routes/index")
+    indexRoutes      = require("./routes/index");
     
 mongoose.connect("mongodb+srv://bae_justin:Megaman911?@cluster0-dpefs.mongodb.net/yelp_camp?retryWrites=true", {useNewUrlParser: true});
 app.use(bodyParser.urlencoded({extended: true}));
