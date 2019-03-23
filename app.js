c2ce41856f333c6ad7cfef33d3f38467405191da
justinkangbae <45549787+justinkangbae@users.noreply.github.com>
2019-03-23 18:16:46 -0500

    update app.js to remove any mention of seeds file

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ var express     = require("express"),
     Campground  = require("./models/campground"),
     Comment     = require("./models/comment"),
     User        = require("./models/user"),
-    seedDB      = require("./seeds"),
     flash       = require("connect-flash");
     
 //requiring routes
@@ -25,7 +24,6 @@ app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
 app.use(flash());
-// seedDB(); //seed the database
 
 // PASSPORT CONFIGURATION
 app.use(require("express-session")({
