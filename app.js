59849c100b08a4668b20c4eaa73839698bc38691
Justin Bae <bae_justin@yahoo.com>
2019-03-23 20:42:52 +0000

    update db url

--- app.js
+++ app.js
@@ -15,8 +15,10 @@ var express     = require("express"),
 var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index");
-    
-mongoose.connect("mongodb+srv://bae_justin:megaman@cluster0-dpefs.mongodb.net/yelp_camp?retryWrites=true", {useNewUrlParser: true});
+
+mongoose.connect(process.env.DATABASEURL, {useNewUrlParser: true});
+// mongoose.connect("mongodb+srv://bae_justin:megaman@cluster0-dpefs.mongodb.net/yelp_camp?retryWrites=true", {useNewUrlParser: true});
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
